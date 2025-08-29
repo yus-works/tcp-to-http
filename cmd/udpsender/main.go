229114954362cfdb27737fbe88d5870dc1cb8950
teodord25 <djuric.teodor25@gmail.com>
2025-08-29 12:45:38 +0200

    udp demo

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	raddr, err := net.ResolveUDPAddr("udp", "localhost:42069")
+	if err != nil {
+		log.Println("Error resolving address: ", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, raddr)
+	if err != nil {
+		log.Println("Error dialing udp connection: ", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			log.Println("Error reading from stdio: ", err)
+		}
+
+		_, err = conn.Write(line)
+		if err != nil {
+			log.Println("Error writing to udp conn: ", err)
+		}
+	}
+}
