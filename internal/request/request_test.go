94ec27fa0539a36d5f3e21a903ccb95c0ca3f176
teodord25 <djuric.teodor25@gmail.com>
2025-08-31 20:16:12 +0200

    test(request): add basic testing

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,55 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	// Test: Good GET Request line
+	r, err := RequestFromReader(
+		strings.NewReader(
+			"GET / HTTP/1.1\r\n" +
+			"Host: localhost:42069\r\n" +
+			"User-Agent: curl/7.81.0\r\n" +
+			"Accept: */*\r\n" +
+			"\r\n",
+		),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = RequestFromReader(
+		strings.NewReader(
+			"GET /coffee HTTP/1.1\r\n" +
+			"Host: localhost:42069\r\n" +
+			"User-Agent: curl/7.81.0\r\n" +
+			"Accept: */*\r\n" +
+			"\r\n",
+		),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid number of parts in request line
+	_, err = RequestFromReader(
+		strings.NewReader(
+			"/coffee HTTP/1.1\r\n" +
+			"Host: localhost:42069\r\n" +
+			"User-Agent: curl/7.81.0\r\n" +
+			"Accept: */*\r\n" +
+			"\r\n",
+		),
+	)
+	require.Error(t, err)
 }
