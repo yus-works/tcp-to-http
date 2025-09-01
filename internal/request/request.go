2f41499f3a1bbeae6835096a086dd80c9404c233
teodord25 <djuric.teodor25@gmail.com>
2025-09-01 22:10:47 +0200

    refactor(request): switch to bytes

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,11 +1,11 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"regexp"
-	"strings"
 )
 
 type Request struct {
@@ -33,11 +33,11 @@ var versions = map[string]struct{}{
 // TODO: use better validation for this
 var isValidTarget = regexp.MustCompile("[*/][-_a-zA-Z0-9]*")
 
-func parseRequestLine(line string) (*RequestLine, error) {
+func parseRequestLine(line []byte) (*RequestLine, error) {
 	var reqLine RequestLine
 	var err error
 
-	parts := strings.Split(line, " ")
+	parts := bytes.Split(line, []byte{' '})
 
 	if len(parts) != 3 {
 		err = fmt.Errorf("Request line must contain exactly 3 parts")
@@ -45,7 +45,7 @@ func parseRequestLine(line string) (*RequestLine, error) {
 		return nil, err
 	}
 
-	method := parts[0]
+	method := string(parts[0])
 	if _, ok := methods[method]; !ok {
 		err = fmt.Errorf("Request METHOD not found in allowed set")
 		log.Println("Invalid METHOD: ", err)
@@ -55,29 +55,30 @@ func parseRequestLine(line string) (*RequestLine, error) {
 	reqLine.Method = method
 
 	target := parts[1]
-	if !isValidTarget.MatchString(target) {
+	if !isValidTarget.Match(target) {
 		err = fmt.Errorf("Request TARGET must follow [/].* (for now)")
 		log.Println("Invalid TARGET: ", err)
 		return nil, err
 	}
 
-	reqLine.RequestTarget = target
+	reqLine.RequestTarget = string(target)
 
 	versionToken := parts[2]
-	versionParts := strings.Split(versionToken, "/")
-	if versionParts[0] != "HTTP" {
+	versionParts := bytes.Split(versionToken, []byte{'/'})
+	if string(versionParts[0]) != "HTTP" {
 		err = fmt.Errorf("Request type must be exactly 'HTTP'")
 		log.Println("Invalid TYPE: ", err)
 		return nil, err
 	}
 
-	if _, ok := versions[versionParts[1]]; !ok {
+	versionNum := string(versionParts[1])
+	if _, ok := versions[versionNum]; !ok {
 		err = fmt.Errorf("Request version must be exactly '1.1' (for now)")
 		log.Println("Invalid VERSION: ", err)
 		return nil, err
 	}
 
-	reqLine.HttpVersion = versionParts[1]
+	reqLine.HttpVersion = versionNum
 
 	return &reqLine, nil
 }
@@ -89,7 +90,7 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		return nil, err
 	}
 
-	line := strings.Split(string(msg), "\r\n")[0]
+	line := bytes.Split(msg, []byte{'\r', '\n'})[0]
 
 	reqLine, err := parseRequestLine(line)
 	if err != nil {
