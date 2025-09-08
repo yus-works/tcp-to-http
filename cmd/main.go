4124c3864e042c928b7da10aa27c61a664c80e2f
teodord25 <djuric.teodor25@gmail.com>
2025-09-08 11:31:15 +0200

    feat(server): impl basic http server w/ fixed msg

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/yus-works/tcp-to-http/internal/tcplistener"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/yus-works/tcp-to-http/internal/server"
+)
+
+const port = 42069
 
 func main() {
-	tcplistener.Start()
+	server, err := server.Serve(port)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+	defer server.Close()
+	log.Println("Server started on port", port)
+
+	sigChan := make(chan os.Signal, 1)
+
+	// if sigint or sigterm detected, relay it to sigChan
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigChan // block until sigChan has something to produce
+	log.Println("Server gracefully stopped")
 }
+
---
+++ internal/server/server.go
@@ -0,0 +1,75 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+
+	"github.com/yus-works/tcp-to-http/internal/request"
+)
+
+type Server struct {
+	port int
+
+	closed   atomic.Bool
+	listener net.Listener
+}
+
+func Serve(port int) (*Server, error) {
+	ln, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Printf("Error starting listener on %d: %s\n", port, err)
+		return nil, err
+	}
+
+	s := Server{
+		port:     port,
+		listener: ln,
+	}
+
+	go s.listen()
+
+	return &s, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for !s.closed.Load() {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Println("Error accepting connection: ", err)
+			continue
+		}
+
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	_, err := request.RequestFromReader(conn)
+	if err != nil {
+		log.Println("Failed to parse/read request: ", err)
+
+        conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+        return
+	}
+	
+    // Fix: Proper HTTP response format
+    response := "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: 13\r\n" +
+                "\r\n" +
+                "Hello Gamers!"
+
+	conn.Write([]byte(response))
+}
