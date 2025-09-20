e0c22460a96227458523f023c2dc4be08bd65a16
teodord25 <djuric.teodor25@gmail.com>
2025-09-20 16:03:42 +0200

    docs(todo): rm

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -260,63 +260,6 @@ func TestRequestLineParse(t *testing.T) {
 		},
 	)
 	require.Error(t, err)
-
-	// TODO: enable these tests
-
-	// Test: Long request target
-	// r, err = RequestFromReader(
-	// 	&chunkReader{
-	// 		data:"GET /very/long/path/with/many/segments/and/query?param1=value1&param2=value2&param3=value3 HTTP/1.1\r\n" +
-	// 		"Host: localhost:42069\r\n" +
-	// 		"\r\n",	numBytesPerRead: 4,
-	// 	},
-	// )
-	// require.NoError(t, err)
-	// require.NotNil(t, r)
-	// assert.Equal(t, "GET", r.RequestLine.Method)
-	// assert.Equal(t, "/very/long/path/with/many/segments/and/query?param1=value1&param2=value2&param3=value3", r.RequestLine.RequestTarget)
-	// assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
-
-	// Test: legacy version
-	// r, err = RequestFromReader(
-	// 	&chunkReader{
-	// 		data:"GET /legacy HTTP/1.0\r\n" +
-	// 		"Host: localhost:42069\r\n" +
-	// 		"\r\n",	numBytesPerRead: 1,
-	// 	},
-	// )
-	// require.NoError(t, err)
-	// require.NotNil(t, r)
-	// assert.Equal(t, "GET", r.RequestLine.Method)
-	// assert.Equal(t, "/legacy", r.RequestLine.RequestTarget)
-	// assert.Equal(t, "1.0", r.RequestLine.HttpVersion)
-
-	// Test: versioned method
-	// r, err = RequestFromReader(
-	// 	&chunkReader{
-	// 		data:"PATCH-V1.2 /path HTTP/1.1\r\n" +
-	// 		"Host: localhost:42069\r\n" +
-	// 		"\r\n",	numBytesPerRead: 2,
-	// 	}
-	// )
-	// require.NoError(t, err)
-	// require.NotNil(t, r)
-	// assert.Equal(t, "PATCH-V1.2", r.RequestLine.Method)
-
-	// Test: PUT method with query parameters
-	// _, err = RequestFromReader(
-	// 	&chunkReader{
-	// 		data:"PUT /users/123?active=true HTTP/1.1\r\n" +
-	// 		"Host: localhost:42069\r\n" +
-	// 		"Content-Type: application/json\r\n" +
-	// 		"\r\n",	numBytesPerRead: 3,
-	// 	},
-	// )
-	// require.NoError(t, err)
-	// require.NotNil(t, r)
-	// assert.Equal(t, "PUT", r.RequestLine.Method)
-	// assert.Equal(t, "/users/123?active=true", r.RequestLine.RequestTarget)
-	// assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 }
 
 func TestRequestParseHeaders(t *testing.T) {
